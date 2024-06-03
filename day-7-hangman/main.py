52dbcbb5375a5af780e8c622fb8b590e5de9b300
jamesweisner <james.weisner@gmail.com>
2024-06-02 23:55:48 -0700

    day 7

---
+++ day-7-hangman/main.py
@@ -0,0 +1,42 @@
+from os import system
+from random import choice
+from hangman_art import logo, stages
+from hangman_words import word_list
+
+answer = list(choice(word_list).upper())
+display = ['_'] * len(answer)
+history = []
+lives = 6
+error = ''
+
+while True:
+
+    # Draw the current state of the game.
+    system('clear')
+    print(logo)
+    print(stages[lives])
+    print(f"   -= {' '.join(display)} =-\n")
+    print(f"History: {' '.join(history)}")
+    if lives == 0:
+        print('You lose!')
+        break
+    if '_' not in display:
+        print('You win!')
+        break
+
+    # Let the player guess a letter.
+    print(f'{error}')
+    guess = input('Guess a letter: ').upper()
+    if len(guess) != 1:
+        error = 'Please type one letter.'
+        continue
+    if guess in history:
+        error = 'You already guessed that letter.'
+        continue
+    error = ''
+    history.append(guess)
+    for i, letter in enumerate(answer):
+        if letter == guess:
+            display[i] = letter
+    if guess not in answer:
+        lives -= 1
